25ee40017d3fb7498be5482ad52cf2d3fbe62ae0
aiswarya309 <aishuzz309@gmail.com>
2022-10-11 13:14:20 +0530

    login_redux

--- src/Redux/signup/signupMiddle.tsx
+++ src/Redux/signup/signupMiddle.tsx
@@ -3,7 +3,7 @@ import {SignupAction} from './signupAction'
 
 function SignupMiddle(email:any,password:any,name:any){
     return function(dispatch:any){
-        axios.post(`${process.env.REACT_APP_SIGNUP_API}/auth/signup`,{email,password,name})
+        axios.post(`${process.env.REACT_APP_BACKEND}/auth/signup`,{email,password,name})
         .then(response=>{
         dispatch(SignupAction(response.data))
         })
